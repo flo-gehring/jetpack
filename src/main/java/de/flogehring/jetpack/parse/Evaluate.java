22204f852fd862e276f3fa367340f79975ebf942
Flo Gehring <florian.gehring@pm.me>
2025-02-16 11:21:19 +0100

    New Try Complex Left Recursion based on new paper

--- src/main/java/de/flogehring/jetpack/parse/Evaluate.java
+++ src/main/java/de/flogehring/jetpack/parse/Evaluate.java
@@ -74,14 +74,18 @@ public class Evaluate {
             Symbol.NonTerminal nonTerminal
     ) {
         MemoTableKey key = new MemoTableKey(nonTerminal.name(), currentPosition);
-        var lookup = parsingState.lookup(key);
+        final LookupTable lookupTable = parsingState.getLookup();
+
+        var lookup = lookupTable.get(key);
         return switch (lookup) {
             case MemoTableLookup.NoHit() -> {
                 Expression ruleDef = grammar.apply(nonTerminal);
+                lookupTable.insertFailure(key);
                 var ans = evaluateExpression(ruleDef, input, currentPosition, grammar, parsingState);
                 switch (ans) {
-                    case Either.This<ConsumedExpression, String>(var consumed) -> parsingState.getLookup().insertSuccess(key, consumed.parsePosition());
-                    case Either.Or<ConsumedExpression, String> _ -> parsingState.getLookup().insertFailure(key);
+                    case Either.This<ConsumedExpression, String>(var consumed) ->
+                            lookupTable.insertSuccess(key, consumed.parsePosition());
+                    case Either.Or<ConsumedExpression, String> _ -> lookupTable.insertFailure(key);
                 }
                 yield ans;
             }
